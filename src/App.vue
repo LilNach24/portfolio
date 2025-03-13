8a60df6b3afbdd1115158ac88f50804f21881e3a
LilNach24 <delatorreplanas@gmail.com>
2025-03-13 19:26:13 +0100

    feat: implement TheNavbar component and update HomeView

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,9 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import TheNavbar from '@/components/TheNavbar.vue'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
+  <TheNavbar />
   <RouterView />
 </template>
