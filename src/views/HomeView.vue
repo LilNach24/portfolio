a3d2b6109208fdd633d22a18363e6c90215deb41
LilNach24 <delatorreplanas@gmail.com>
2025-03-14 19:09:37 +0100

    feat: add TheStudies component to HomeView and implement custom background animation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,32 @@
 <script setup lang="ts">
-
+import TheStudies from '@/components/TheStudies.vue'
 </script>
 
 <template>
+  <div class="flex flex-row mt-10">
 
+    <TheStudies />
+  </div>
 </template>
+
+<style>
+.custom-bg {
+  background: linear-gradient(-45deg, #63e578, #57cfb3, #eee48a, #65e9ca);
+  background-size: 400% 400%;
+  animation: gradient 15s ease infinite;
+}
+
+@keyframes gradient {
+  0% {
+    background-position: 0% 50%;
+  }
+
+  50% {
+    background-position: 100% 50%;
+  }
+
+  100% {
+    background-position: 0% 50%;
+  }
+}
+</style>
