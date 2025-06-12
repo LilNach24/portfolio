b8f003be02a3b0d886d307c8ad6d39ab11de0f9f
nacho.delatorre@ext.saint-gobain.com <nacho.delatorre@ext.saint-gobain.com>
2025-06-12 12:49:51 +0200

    feat: add mkcert plugin, update navbar for internationalization, and enhance HomeView layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,15 +5,13 @@ import TheNavbar from '@/components/TheNavbar.vue'
 </script>
 
 <template>
-  <div
+  <TheNavbar />
+  <div id="home"
     class="bg-gradient-to-r from-[#291ca0] via-[#c4712d] to-[#a325d4] bg-[length:400%_400%] animate-gradient min-h-screen w-full"
   >
-    <TheNavbar />
-    <div class="">
+    <div class="flex items-center justify-center min-h-screen sm:p-10">
       <ThePersonalCard />
     </div>
   </div>
-  <div class="min-h-screen">
-    
-  </div>
+  <div id="about" class="min-h-screen"></div>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
+import mkcert from 'vite-plugin-mkcert'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    mkcert(),
     vueDevTools(),
     tailwindcss(),
   ],
